e7485837f46499ed1852f52336c180f72a9d2bcb
jackloague1 <jloague@csu.fullerton.edu>
2024-03-21 18:14:25 -0700

    Added basic section headers and started introduction section.

--- app.js
+++ app.js
@@ -13,3 +13,51 @@ window.onscroll = function()
         navbar.classList.remove("sticky-navbar");
     }
 }
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link)
+{
+    link.addEventListener("click", function(e)
+    {
+        e.preventDefault();
+
+        // Gets the href attirubte of the element that is being clicked.
+        // Slice is used to cut off the first character (#) from the href.
+        const id = e.currentTarget.getAttribute("href").slice(1);
+
+        // Gets the object that has an id matching the value of the id variable above.
+        const element = document.getElementById(id);
+
+        // Gets the height of the navigation bar element on the web page.
+        const navHeight = navbar.getBoundingClientRect().height;
+        
+        // const linksHeight = links.getBoundingClientRect().height;
+
+        // Represents the position the web page will scroll to when a link in the
+        // navigation bar is clicked. The height of the navigation bar is subtracted
+        // from the top of the object the web page will scroll to so the navigation
+        // bar does not cover the top of the object.
+        let position = element.offsetTop - navHeight;
+
+        const stickyNavbar = navbar.classList.contains("sticky-navbar");
+
+        // If there is no sticky-navbar object currently on the web page, make sure
+        // to subtract the height of the navigation bar from the position variable so
+        // that the navigation bar does not cover the top of the object.
+        if (stickyNavbar == false)
+        {
+            position = position - navHeight;
+        }
+
+        // Scrolls to the appropriate object on the web page, stopping when the top
+        // of the page reaches the value of the position variable.
+        window.scrollTo(
+            {
+                top: position,
+            }
+        );
+
+        // links.style.height = 0;
+    });
+});
